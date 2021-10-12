8fb1be6f5681b70652824c4bfa03ab571c2c3808
Kenan Casey <kenancasey@gmail.com>
2021-10-12 10:09:02 -0500

    Rename unit test

---
+++ UnitTests/UnitTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticeExercise4;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestStack()
+        {
+        }
+
+        [TestMethod]
+        public void TestQueue()
+        {
+        }
+
+        [TestMethod]
+        public void TestDeque()
+        {
+        }
+
+    }
+}
